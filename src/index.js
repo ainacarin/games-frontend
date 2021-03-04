43b72fb88cbb0d5d9c38feb700ea93f027516be4
Aida N <nacarinobazaga@gmail.com>
2021-03-04 11:16:40 +0100

    Añado visualización de lista de juegos con carátula y título. Añado sass en package

--- src/index.js
+++ src/index.js
@@ -88,5 +88,15 @@ function generateCommentSnippet(comment) {
 
 // TODO: #3 Dar más información que una lista (imagen, nombre, primeros 100 caracteres de summary)
 function generateGameSnippet(game) {
-  return `<a href="/detail.html?id=${game.id}">${game.name}</a>`;
+  // return `<a href="/detail.html?id=${game.id}">${game.name}</a>`;
+  //en lugar de generar una lista, se crea un contenedor con el link para darle estilo luego
+  return `<div class="game-snippet">
+  <a href="/detail.html?id=${game.id}">
+    <div class="game-snippet-container">
+      <img class="img-thumbnail" id="game-${game.id}" src="${game.image}" alt="Cover of game ${game.name}" />
+    </div>
+    <h3 class="game-name">${game.name}</h3>
+  </a>
+</div>`;
+
 }
