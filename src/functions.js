5e09462ddbe58e5abbf6e6ef194deb02538eceb3
Aida N <nacarinobazaga@gmail.com>
2021-03-04 20:19:00 +0100

    Refactorizar código con archivo functions

---
+++ src/functions.js
@@ -0,0 +1,37 @@
+
+export function generateCommentSnippet(comment) {
+    return `<div class="row">
+    <div class="col col-2">
+      <div class="image-container text-right">
+        <img class="avatar rounded" src="${comment.user.image}" alt="${comment.user.name}" />
+      </div>
+    </div>
+    <div class="col col-10">
+      <div class="user-name-container">
+        <h4 class="text-muted">${comment.user.name}</h4>
+      </div>
+      <div class="comment-body-container">
+        ${comment.body}
+      </div>
+      <div class="date-container">
+        Comment date: <b>${comment.commentDate}</b>
+      </div>
+    </div>
+  </div>`;
+  
+  }
+  
+  // TODO: #3 Dar más información que una lista (imagen, nombre, primeros 100 caracteres de summary)
+ export function generateGameSnippet(game) {
+    // return `<a href="/detail.html?id=${game.id}">${game.name}</a>`;
+    //en lugar de generar una lista, se crea un contenedor con el link para darle estilo luego
+    return `<div class="game-snippet">
+    <a href="/detail.html?id=${game.id}">
+      <div class="game-snippet-container">
+        <img class="img-thumbnail" id="game-${game.id}" src="${game.image}" alt="Cover of game ${game.name}" />
+      </div>
+      <h3 class="game-name">${game.name}</h3>
+    </a>
+  </div>`;
+  
+  }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { generateCommentSnippet, generateGameSnippet } from './functions.js'
+
 const endpoint = 'http://localhost:3000';
 
 //TODO: #1 implementar api. Mirar en documentación
@@ -64,39 +66,39 @@ async function drawComments(gameId) {
   }
 }
 
-function generateCommentSnippet(comment) {
-  return `<div class="row">
-  <div class="col col-2">
-    <div class="image-container text-right">
-      <img class="avatar rounded" src="${comment.user.image}" alt="${comment.user.name}" />
-    </div>
-  </div>
-  <div class="col col-10">
-    <div class="user-name-container">
-      <h4 class="text-muted">${comment.user.name}</h4>
-    </div>
-    <div class="comment-body-container">
-      ${comment.body}
-    </div>
-    <div class="date-container">
-      Comment date: <b>${comment.commentDate}</b>
-    </div>
-  </div>
-</div>`;
+// function generateCommentSnippet(comment) {
+//   return `<div class="row">
+//   <div class="col col-2">
+//     <div class="image-container text-right">
+//       <img class="avatar rounded" src="${comment.user.image}" alt="${comment.user.name}" />
+//     </div>
+//   </div>
+//   <div class="col col-10">
+//     <div class="user-name-container">
+//       <h4 class="text-muted">${comment.user.name}</h4>
+//     </div>
+//     <div class="comment-body-container">
+//       ${comment.body}
+//     </div>
+//     <div class="date-container">
+//       Comment date: <b>${comment.commentDate}</b>
+//     </div>
+//   </div>
+// </div>`;
 
-}
+// }
 
-// TODO: #3 Dar más información que una lista (imagen, nombre, primeros 100 caracteres de summary)
-function generateGameSnippet(game) {
-  // return `<a href="/detail.html?id=${game.id}">${game.name}</a>`;
-  //en lugar de generar una lista, se crea un contenedor con el link para darle estilo luego
-  return `<div class="game-snippet">
-  <a href="/detail.html?id=${game.id}">
-    <div class="game-snippet-container">
-      <img class="img-thumbnail" id="game-${game.id}" src="${game.image}" alt="Cover of game ${game.name}" />
-    </div>
-    <h3 class="game-name">${game.name}</h3>
-  </a>
-</div>`;
+// // TODO: #3 Dar más información que una lista (imagen, nombre, primeros 100 caracteres de summary)
+// function generateGameSnippet(game) {
+//   // return `<a href="/detail.html?id=${game.id}">${game.name}</a>`;
+//   //en lugar de generar una lista, se crea un contenedor con el link para darle estilo luego
+//   return `<div class="game-snippet">
+//   <a href="/detail.html?id=${game.id}">
+//     <div class="game-snippet-container">
+//       <img class="img-thumbnail" id="game-${game.id}" src="${game.image}" alt="Cover of game ${game.name}" />
+//     </div>
+//     <h3 class="game-name">${game.name}</h3>
+//   </a>
+// </div>`;
 
-}
+// }
