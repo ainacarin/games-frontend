d5efacb4ced710f73115c89fe36320392aca199a
Aida N <nacarinobazaga@gmail.com>
2021-03-08 18:29:48 +0100

    Refactorizo el fichero de configuración del webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,50 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
+const plugins = [
+  new HtmlWebpackPlugin({
+    template: './index.html',
+    filename: 'index.html',
+    chunks: ['main']
+  }),
+  new HtmlWebpackPlugin({
+    template: './detail.html',
+    filename: 'detail.html',
+    chunks: ['detail']
+  }),
+  new MiniCssExtractPlugin({
+    filename: '[name].[contenthash].css'
+  })
+];
+
+const loaders = [
+  {
+    // Loader para scss
+    test: /\.s[ac]ss$/i,
+    use: [
+      // Creates `style` nodes from JS strings
+      // "style-loader",
+      // cambio el loader para poner el loader propio del plugin para css
+      MiniCssExtractPlugin.loader,
+      // Translates CSS into CommonJS
+      "css-loader",
+      // Compiles Sass to CSS
+      "sass-loader",
+    ],
+  },
+  {
+    // Loader para versiones diferentes de navegadores 
+    test: /\.m?js$/,
+    exclude: /(node_modules|bower_components)/,
+    use: {
+      loader: 'babel-loader',
+      // options: {
+      //   presets: ['@babel/preset-env']
+      // } //se pone en el archivo de configuración .babelrc
+    },
+  }
+];
+
 module.exports = {
   // punto de partida de todas las rutas para no estar escribiéndolas cte
   context: __dirname + '/src',
@@ -15,49 +59,9 @@ module.exports = {
     clean: true
   },
   module: {
-    rules: [
-      {
-        // Loader para scss
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          // "style-loader",
-          // cambio el loader para poner el loader propio del plugin para css
-          MiniCssExtractPlugin.loader,
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader",
-        ],
-      },
-      {
-        // Loader para versiones diferentes de navegadores 
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          // options: {
-          //   presets: ['@babel/preset-env']
-          // } //se pone en el archivo de configuración .babelrc
-        },
-      }
-    ]
+    rules: loaders
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-      filename: 'index.html',
-      chunks: ['main']
-    }),
-    new HtmlWebpackPlugin({
-      template: './detail.html',
-      filename: 'detail.html',
-      chunks: ['detail']
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].[contenthash].css'
-    })
-  ],
+  plugins: plugins,
   devServer: {
     contentBase: path.join(__dirname, "dist"),
     compress: true,
